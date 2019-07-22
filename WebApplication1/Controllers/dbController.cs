20c610fb892748451fc91ce751a22ffb27d9cc02
Shady Fahmy <shadyfahmy67@gmail.com>
2019-07-22 13:11:07 +0200

    app files

---
+++ WebApplication1/Controllers/dbController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class dbController : ApiController
+    {
+
+        DBHandler handler;
+
+        public dbController()
+        {
+            handler = new DBHandler();
+        }
+
+        // GET: api/db
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/db/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/db
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/db/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/db/5
+        public void Delete(int id)
+        {
+        }
+
+        [Route("api/DB/GetTest/{id:long}")]
+        [HttpGet]
+        public DataTable GetTest(long id)
+        {
+            return handler.GetTest(id);
+        }
+
+        [Route("api/DB/GetTest1")]
+        [HttpGet]
+        public DataTable GetTest1()
+        {
+            return handler.GetTest1();
+        }
+    }
+}
---
+++ WebApplication1/DBHandler/DBHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace WebApplication1
+{
+    public class DBHandler
+    {
+        DBManager dbMan;
+
+        public DBHandler()
+        {
+            dbMan = new DBManager();
+        }
+     
+        public DataTable GetTest(long id)
+        {
+            string query = "select ResponseOID from BoloPersons where OID = " + id;
+            return dbMan.ExecuteReader(query);
+        }
+
+        public DataTable GetTest1()
+        {
+            string query = "SELECT Responses.Type,COUNT(Responses.OID) as 'Number' FROM dbo.Responses group by Responses.Type " ;
+            return dbMan.ExecuteReader(query);
+        }
+    }
+}
